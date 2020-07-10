e4eafbbbf8c996442656b02895388763a4aa3bda
EdgarPDX <66703437+EdgarPDX@users.noreply.github.com>
2020-07-09 20:05:38 -0700

    added function to test

--- test/example.test.js
+++ test/example.test.js
@@ -1,4 +1,5 @@
 // IMPORT MODULES under test here:
+import { countsAsAYes } from '../function.js';
 // import example from '../example.js';
 
 const test = QUnit.test;
@@ -6,11 +7,12 @@ const test = QUnit.test;
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
+    const word = 'Yellow';
     const expected = true;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = countsAsAYes(word);
 
     //Expect
     // Make assertions about what is expected versus the actual result
